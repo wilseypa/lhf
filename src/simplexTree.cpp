44e304a96a3b315e8b7bfaf1638b6bd71d7fb134
Anindya Moitra <id.anindya@gmail.com>
2018-07-05 01:07:41 -0400

    Checking in an implementaion of essential functions of a simplex tree.

--- src/simplexTree.cpp
+++ src/simplexTree.cpp
@@ -1,10 +1,15 @@
 #include <string>
 #include <vector>
+#include <iostream>
 #include "simplexTree.hpp"
 
 // simplexTree constructor
 simplexTree::simplexTree(){
 	simplexType="simplexTree";
+	this->isLeaf = false;
+
+	for (int i = 0; i < MAX_POINTS; i++)
+		this->character[i] = nullptr;
 }
 
 double simplexTree::getSize(){
@@ -16,16 +21,106 @@ double simplexTree::getSize(){
 }
 
 
-void simplexTree::insert(std::vector<double> vector){
-	//insert into the simplex tree
+// Iterative function to insert a simplex, i.e. a key, in the tree.
+void simplexTree::insert(std::string key) {
+
+	// Start from the root.
+	simplexTree* present = this;
+	for (int i = 0; i < key.length(); i++) {
+		// Create a new node if it doesn't exist.
+		if (present->character[key[i]] == nullptr)
+			present->character[key[i]] = new simplexTree();
+
+		// Go to the next node.
+		present = present->character[key[i]];
+	}
 	
-	return;
+	// Set the current node as a leaf.
+	present->isLeaf = true;
+
 }
 
-void simplexTree::find(std::vector<double> vector){
-	//find in the simplex tree
+
+// Iterative function to search for a key in the tree. The function returns true
+// if the key is found, else it returns false.
+bool simplexTree::search(std::string key){
 	
-	return;
+	// Return false if the tree is empty.
+	if (this == nullptr)
+		return false;
+
+	simplexTree* present = this;
+
+	for (int i = 0; i < key.length(); i++) {
+		// Go to the next node.
+		present = present->character[key[i]];
+
+		// If the key queried for is invalid, i.e. if the end of the tree is reached
+		if (present == nullptr)
+			return false;
+	}
+
+	// If the current node is a leaf and we have reached the end of the string, return true.
+	return present->isLeaf;
+
+}
+
+
+// The following function returns true if a given node has a child.
+bool simplexTree::haveChild(simplexTree const* present) {
+	for (int i = 0; i < MAX_POINTS; i++)
+		if (present->character[i])
+			return true;    // Child found.
+
+	return false;
+}
+
+// A recursive function to delete a simplex from the tree.
+bool simplexTree::deletion(simplexTree*& present, std::string key) {
+
+	// Return if the tree is empty
+	if (present == nullptr)
+		return false;
+
+	// If the end of the key is not reached.
+	if (key.length()) {
+		// recursively search for the node corresponding to the next character
+		// in the key and if it returns true, delete current node given it is a non-leaf node.
+		if (present != nullptr &&
+				present->character[key[0]] != nullptr &&
+				deletion(present->character[key[0]], key.substr(1)) &&
+				present->isLeaf == false) {
+
+			if (!haveChild(present)) {
+				delete present;
+				present = nullptr;
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	// If the end of the key is reached
+	if (key.length() == 0 && present->isLeaf) {
+		// If the current node is a leaf node and doesn't have any child
+		if (!haveChild(present)) {
+			// Delete the current node.
+			delete present;
+			present = nullptr;
+
+			// Delete the non-leaf parent nodes.
+			return true;
+		} else {
+			// Mark current node as a non-leaf node (it is not deleted).
+			present->isLeaf = false;
+
+			// Its parent nodes are not deleted.
+			return false;
+		}
+	}
+
+	return false;
 }
 
 int simplexTree::vertexCount(){
--- src/simplexTree.hpp
+++ src/simplexTree.hpp
@@ -1,16 +1,27 @@
 #pragma once
 #include "simplexBase.hpp"
 
+// Define the maximum number of points whose corresponding simplices can be stored
+// in the simplex tree.
+#define MAX_POINTS 2000
+
 // Header file for simplexTree class - see simplexTree.cpp for descriptions
 
 class simplexTree : public simplexBase {
   private:
   public:
 	simplexTree();
+	bool isLeaf;
+	simplexTree* character[MAX_POINTS];
 	
-	double getSize();	
-	void insert(std::vector<double>);
-	void find(std::vector<double>);
+	double getSize();
+
+	// At this time, let's just assume that each simplex is labeled by a key that
+	// can, in general, be considered as a string.
+	void insert(std::string);
+	bool deletion(simplexTree*&, std::string);
+	bool search(std::string);
+	bool haveChild(simplexTree const*);
 	int vertexCount();
 	int simplexCount();
 };
