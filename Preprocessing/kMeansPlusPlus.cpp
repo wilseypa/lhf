3296f9f4955041d08837fe1d3a545f3475eed89b
malottno <malottno@mail.uc.edu>
2019-02-14 14:50:46 -0500

    Updates and restructuring of refinement loop, notes for Aaron

--- Preprocessing/kMeansPlusPlus.cpp
+++ Preprocessing/kMeansPlusPlus.cpp
@@ -44,11 +44,17 @@ pipePacket kMeansPlusPlus::runPreprocessor(pipePacket inData){
     int index = distribution(gen);
 
     
+    std::vector<std::vector<double>> tempCentroids; 
     std::vector<double> center_initial = inData.workData.originalData[index];
     centroids.push_back(center_initial);  //adding first mean to centroids
+    tempCentroids.push_back(center_initial);
     std::vector<double> tempDist;
-    std::vector<std::vector<double>> tempCentroids; 
 
+
+
+	//Determining initial centroids by probability / distance from previous centroid
+	// 	Do we need to compare the picked centroid to all centroids previously picked 
+	//		-> in order to maximize the distance/difference between centroids
      for(unsigned k = 0; k<num_clusters-1; k++){ //adding means 2 -> k-1 to centroids based on distance 
         int index_next = distribution(gen);
         std::vector<double>  center_next = inData.workData.originalData[index_next];
@@ -59,35 +65,90 @@ pipePacket kMeansPlusPlus::runPreprocessor(pipePacket inData){
                 
                 bool valid_center = (rand() %100) < (probability) * 10;
                 
-                if(valid_center){
+                if(valid_center && (std::find(tempCentroids.begin(), tempCentroids.end(), inData.workData.originalData[index_next]) == tempCentroids.end())){
                     tempCentroids.push_back(inData.workData.originalData[index_next]);
                 }
             
             }
     }       
-
-
-        auto end = tempCentroids.end();  //removing duplicates from temp matrix
-        for (auto it = tempCentroids.begin(); it != end; it++){
-            end = std::remove(it +1, end, *it);
-        }
-        tempCentroids.erase(end, tempCentroids.end());
-
-        for(int i = 0; i<tempCentroids.size(); i++){
-            centroids.push_back(tempCentroids[i]);  //adding unique centroids to centroids matrix
-        } 
-        
     
-        for(unsigned i = 0; i < centroids.size(); i++){
-        for(unsigned j = 0; j < centroids[j].size(); j++){
-	    std::cout << i + 1 << "->";
-            std::cout << centroids[i][j]<< '\t';
+    //Now, we need to iterate over the centroids and classify each point
+    //	Compute the new centroid as the geometric mean of the classified points
+    //		IF the centroid has 0 points classified to it, we need to repick/reapproach (find out how)
+    //	Replace old centroids with new centroids, rinse, repeat until convergence
+    //		Convergence on # iterations, or a minimum movement of centroids (< .01)
+    // OUTPUT: Final centroids, labeled data, sum of vectors in a label, count of points in label
+    
+    std::vector<double> counts(num_clusters, 0);
+    std::vector<double> summed_clusters(num_clusters, 0); 
+    
+	for (int z = 0; z < num_iterations; z++){
+		int minDistanceLabel = 0;
+		double minDist = std::numeric_limits<double>::max();
+		for (int c = 0; c < tempCentroids.size(); c++){
+			int minDistanceLabel = 0;
+			
+			for ( int j = 0; j < inData.workData.originalData.size(); j++){
+				auto curDist = ut.vectors_distance(inData.workData.originalData[j], tempCentroids[c]);
+				
+				
+				if(curDist < minDist){
+					minDistanceLabel = c;
+					minDist = curDist;
+				}
+				
+				
+			}
+			
+			counts[minDistanceLabel] ++;
+			summed_clusters[c] += minDist;
+			
+			//Compute feature sum of distances for a label ** Nick to check on this and revise** 
+			// summed_features[label] += ut.featureDistance(originalData, tempCentroids)
+			
+		}		
+		
+		//if z=num_iterations or converge: 
+		//	Assign Final Labels
+		//	Assign final centroid points (geometric center of classified points)
+		//  Rmax, Ravg
+		//	BREAK
+		
+		
+		//Otherwise, 
+		//		Shift the centroid geometric centers to new centroids
+		
+	}
+   
+   
 
-        }
-        std::cout << std::endl;
-        
-        }
-        //labeling points so they can be assigned to a cluster
+	/*std::cout << tempCentroids.size() << " : " << tempCentroids[0].size() << std::endl;
+
+	auto end = tempCentroids.end();  //removing duplicates from temp matrix
+	for (auto it = tempCentroids.begin(); it != end; it++){
+		end = std::remove(it +1, end, *it);
+	}
+	tempCentroids.erase(end, tempCentroids.end());
+	
+	
+	std::cout << tempCentroids.size() << " : " << tempCentroids[0].size() << std::endl;
+*/
+
+	for(int i = 0; i<tempCentroids.size(); i++){
+		centroids.push_back(tempCentroids[i]);  //adding unique centroids to centroids matrix
+	} 
+
+
+	for(unsigned i = 0; i < centroids.size(); i++){
+	for(unsigned j = 0; j < centroids[j].size(); j++){
+	std::cout << i + 1 << "->";
+		std::cout << centroids[i][j]<< '\t';
+
+	}
+	std::cout << std::endl;
+
+	}
+	//labeling points so they can be assigned to a cluster
     //  This will eventually occur in parallel, should probably be removed (eventually)
     //  i.e. labels are not necessary to continue the PH computation but are for upscaling
 
@@ -137,8 +198,6 @@ pipePacket kMeansPlusPlus::runPreprocessor(pipePacket inData){
 	}   */
 
     //counting points in each cluster so mean can be computed
-    std::vector<double> counts(num_clusters, 0);
-    std::vector<double> summed_clusters; 
   
     for(size_t point=0; point<inData.workData.originalData.size(); point++){   
         summed_clusters.push_back(0);
@@ -153,7 +212,7 @@ pipePacket kMeansPlusPlus::runPreprocessor(pipePacket inData){
 	std::cout << std::endl;
 	for(unsigned j = 0; j < counts.size(); j++){
 		std::cout << counts[j] << '\t';
-            std::cout << summed_clusters[j] << "\trAvg = " << summed_clusters[j]/counts[j] << std::endl;
+        std::cout << summed_clusters[j] << "\trAvg = " << summed_clusters[j]/counts[j] << std::endl;
 	} 
 
 
