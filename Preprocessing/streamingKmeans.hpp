87601b20c46b364671b187c25c100118c529028b
aaaronsens <asens1337@gmail.com>
2019-06-06 23:04:39 -0400

    fixed approxFacilities labeling and sorting

--- Preprocessing/streamingKmeans.hpp
+++ Preprocessing/streamingKmeans.hpp
@@ -13,8 +13,8 @@ class streamingKmeans : public preprocessor {
 	streamingKmeans();
   double dotProd(const std::vector<double>& a, const std::vector<double>& b);
   double dotProd2D(std::vector<std::vector<double>>& a, std::vector<std::vector<double>> & b);
-  std::vector<double> approxNearestNeighbor(std::vector<std::vector<double>> facilities, std::vector<double> approxFacilities, std::vector<double> facilityLabel, std::vector<double> omega, int x, int size, pipePacket(inData));
-  int binarySearch(std::vector<double> approxFacilities, std::vector<double> omega, int n, double target);
+  std::vector<double> approxNearestNeighbor(std::vector<std::vector<double>> facilities, std::vector< std::pair <double, int> > sortedApproxFacils,  std::vector<double> omega, int x, int size, pipePacket(inData));
+  int binarySearch(std::vector< std::pair <double, int> > sortedApproxFacils, std::vector<double> omega, int n, double target);
   double randDouble(); // Returns a random double in the range [0,1)
   bool prob(double f); 
   int random(int low, int high);   // returns random int in the range [low, high)
