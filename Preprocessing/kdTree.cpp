f4757151b2f657cb886782635998fb368d3a49e2
aaron <asens1337@gmail.com>
2019-10-24 13:20:01 -0400

    construct kdtree
    added appropriate level of abstraction so that it will be quick to implement neighborhood search and return functionality. taking advantage of aliases for naming. using smart pointers to avoid memory leaks

--- Preprocessing/kdTree.cpp
+++ Preprocessing/kdTree.cpp
@@ -3,6 +3,8 @@
  * pipeline functions to execute
  *
  */
+#include <stdio.h>
+#include <fstream>
 #include <algorithm>
 #include <cstdlib>
 #include <limits>
@@ -13,13 +15,15 @@
 #include <iostream>
 #include <functional>
 #include <vector>
+#include <queue>
+
 #include "kdTree.hpp"
 #include "utils.hpp"
 
-/*kdTree::kdTree(){
-    procName = "kdTree";
+kdTree::kdTree(){
+ //   procName = "kdTree";
     return;
-} */
+} 
 // create a binary tree of nodes of "k" dimensions from points of the input csv
 //every leaf node is a k dimensional point, non leaf nodes splits hyperplane of space
 // each node represents axis aligned hyper rectangle 
@@ -31,44 +35,137 @@
 
 //kdtree is used to find nearest neighbors to current point based on epsilon
 
+//avdongre.wordpress.com/2011/06/14/kd-tree-in-c/
+// github.com/crvs/KDTree/blob/master/KDTree.cpp#L41
 
+using point =std::vector<double>;
+using pointList = std::vector<std::vector<double>>;
+using pointListItr =  pointList::iterator; //alias declarations
 
+kdNode::kdNode() = default;
 
-//input: List of points (inData), depth ( O log(n))
-std::vector<double> a{ 1.1, 2.2, 1.7, 5.6, 1.123, 9.1};
-std::vector<double> b{3.34, 5.23, .532, 1.12, 3.45, 1.23};
-std::vector<std::vector<double>> pointList { a , b};
-int depth = log2(pointList.size());
-int k = pointList[0].size();
-
-//select axis based on depth so that it cycles through all valid vals
-int axis = depth%k; 
-
-
-// sort pointList and choose median as pivot element
-//std::sort(pointList[axis].begin(), pointList[axis].end());
-
-
-//create node and construct subtree
-/* node location = median
-leftChild = kdtree( points in pointList before median, depth + 1)
-rightChild = kdtree(points in pointList after median, depth +1)
-return node en.wikipedia.org/wiki/K-d_tree#Construction
-*/
-
-//node has 5 fields: splitting axis, split val, L/R subtree, point (if leaf)
-
-
-
-
-
-
-// runPipe -> Run the configured functions of this pipeline segment
-pipePacket kdTree::runPreprocessor(pipePacket inData)
+kdNode::kdNode(const point &pt, const size_t &idx_, const kdNodePtr &left_,
+               const kdNodePtr &right_)
 {
+    x = pt;
+    index = idx_;
+    left = left_;
+    right = right_;
+}
+
+kdNode::kdNode(const pointIndex &pi, const kdNodePtr &left_,
+               const kdNodePtr &right_)
+{
+    x = pi.first;
+    index = pi.second;
+    left = left_;
+    right = right_;
+}
+
+kdNode::~kdNode() = default; //destructor
+
+kdNodePtr newKdNodePtr()
+{
+    kdNodePtr mynode = std::make_shared<kdNode>();
+    return mynode;
+}
+
+kdNodePtr kdTree::makeTree(const pointIndexArr::iterator &begin, //
+                           const pointIndexArr::iterator &end,   //
+                           const size_t &length,                 //
+                           const size_t &level){
+    if(begin == end){
+        return newKdNodePtr(); // sanity check for empty list
+    }
+
+    size_t dim = begin->first.size();
+
+    if(length > 1){
+        std::sort(begin, end, level);
+    }
+
+    auto middle = begin + (length/2);
+
+    auto leftBegin = begin;
+    auto leftEnd = middle;
+    auto rightBegin = middle + 1;
+    auto rightEnd = end;
+
+    size_t leftLength = length/2;
+    size_t rightLength = length - leftLength -1;
+
+    kdNodePtr left; //recursively build left and right branches until leaf reached
+    if (leftLength > 0 && dim > 0){
+        left = makeTree(leftBegin, leftEnd, leftLength, (level+1) % dim);
+    }
+    else{
+      //  left = leaf;
+    }
+
+    kdNodePtr right; 
+    if(rightLength > 0 && dim > 0){
+        right = makeTree(rightBegin, rightEnd, rightLength, (level+1)% dim);
+    }
+    else{
+     //   right = leaf;
+    }
+
+
+
+    return std::make_shared< kdNode >(*middle, left, right);
+    //returns pointer to root of the constructed kd tree
+
+
+
 
 }
 
+//driver code, push back inData.originalData to point vec so we can put in tree
+kdTree::kdTree(pointVec pointArray, pipePacket inData){
+    kdNodePtr leaf = std::make_shared< kdNode>();
+    pointIndexArr arr;
+
+    for(size_t i=0; i<inData.originalData.size(); i++){
+        arr.push_back(pointIndex(inData.originalData[i],  i));
+    }
+    
+    auto begin = arr.begin();
+    auto end = arr.end();
+    
+    size_t length = arr.size();
+    size_t level = 0; 
+
+    root = kdTree::makeTree(begin, end, length, level);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     /*pipePacket kdTree::runPreprocessor(pipePacket inData){
+
+} 
+ 
 
 
 
@@ -90,4 +187,4 @@ bool kdTree::configPreprocessor(std::map<std::string, std::string> configMap)
 
   //  ut.writeDebug("StreamKMeans", "Configured with parameters { clusters: " + configMap["clusters"] + ", iterations: " + configMap["iterations"] + ", debug: " + strDebug + ", outputFile: " + outputFile + " }");
 
-}
+}  */
--- Preprocessing/kdTree.hpp
+++ Preprocessing/kdTree.hpp
@@ -3,14 +3,105 @@
 // Header file for kdTree class - see kdTree.cpp for descriptions
 #include <map>
 #include "preprocessor.hpp"
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <vector>
 
-class kdTree : public preprocessor
+//github.com/crvs/KDTree/blob/master/KDTree.hpp
+using point = std::vector<double>;
+using pointList = std::vector<std::vector<double>>;
+using pointListItr = pointList::iterator; 
+using indexArr = std::vector< size_t >;
+using pointIndex = typename std::pair<std::vector<double>, size_t>;  //alias declarations
+
+
+class kdNode {
+    public: 
+        using kdNodePtr = std::shared_ptr< kdNode >; //initializer smart pointer
+        size_t index;
+        point x;
+        kdNodePtr left; 
+        kdNodePtr right;
+
+
+        kdNode();
+        kdNode(const point &, const size_t &, const kdNodePtr &, const kdNodePtr &);
+        kdNode(const pointIndex &, const kdNodePtr &, const kdNodePtr &);
+        ~kdNode(); //destructor
+
+};
+
+using kdNodePtr = std::shared_ptr< kdNode >;
+
+kdNodePtr newkdNodePtr();
+
+using pointIndexArr = typename std::vector<pointIndex>;
+using pointVec = std::vector<point>;
+
+class kdTree{
+    kdNodePtr root;
+    kdNodePtr leaf;
+
+    kdNodePtr makeTree(const pointIndexArr::iterator &begin, const pointIndexArr::iterator &end, 
+                       const size_t &length, const size_t &level);
+    public:
+        kdTree() = default;
+       explicit kdTree(pointVec pointArray, pipePacket inData); //vector of points, which are std::vector<doubles>. prevent implicit conversion
+
+    private:
+      kdNodePtr nearest(
+          const kdNodePtr &branch,
+          const point &pt,
+          const size_t &level,
+          const kdNodePtr &best,
+          const double &bestDist
+      );
+
+    kdNodePtr nearest(const point &pt);
+
+    public:
+        point nearestPoint(const point &pt);
+        size_t nearestIndex(const point &pt);
+        pointIndex nearestPointIndex(const point &pt);
+    
+    private:
+        pointIndexArr neighborhood_(
+            const kdNodePtr &branch,
+            const point &pt,
+            const double &rad, //epsilon
+            const size_t &level
+        );
+
+    public:
+        pointIndexArr neighborhood( //
+            const point &pt,      //
+            const double &rad);
+
+        pointVec neighborhood_points( //
+            const point &pt,        //
+            const double &rad);
+
+        indexArr neighborhood_indices( //
+            const point &pt,         //
+            const double &rad);
+};
+
+/*class kdTree : public preprocessor
 {
 private:
   
 
 public:
-  
+    void makePointList(pipePacket inData);
+    kdNodePtr makeTree(const pointIndexArr::iterator &begin, //
+                       const pointIndexArr::iterator &end,   //
+                       const size_t &length,                 //
+                       const size_t &level);
+
     pipePacket runPreprocessor(pipePacket inData);
     bool configPreprocessor(std::map<std::string, std::string> configMap);
-};
+};  */
+
+
+
