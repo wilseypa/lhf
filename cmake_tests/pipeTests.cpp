6bea4347537ba2a359cca823c1627afde1335a28
malottno <nick@malott.us>
2020-01-06 19:17:35 -0500

    Adding pipe tests, updating workflows to run on new branch

---
+++ cmake_tests/pipeTests.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include "basePipe.hpp"
+#include "pipePacket.hpp"
+
+// TEST basePipe Functions
+void t_pipe_functions(std::string &log){
+	std::string failLog = "";
+	auto *pack = new pipePacket("simplexArrayList", 5.0, 2);
+	basePipe *testPipe = new basePipe();
+	
+	std::map<std::string, std::string> testConfig;
+	
+	//Attempt to output pipe data without initializing pipe
+	// RET: void 
+	testPipe->outputData(*pack);
+	
+	//Attempt to run the pipe without initializing
+	// RET: void
+    testPipe->runPipe(*pack);
+    
+    //Configure the pipe with arguments
+    // RET: bool
+    if(!testPipe->configPipe(testConfig)){ failLog += "basePipe config failed\n"; }
+	
+	//Run the pipe wrapper with no selected pipe
+    testPipe->runPipeWrapper(*pack);
+	
+	//Output log status to calling function
+	if(failLog.size() > 0){
+		log += "FAILED: basePipe Test Functions---------------------------\n" + failLog;	
+	} else {
+		 log += "PASSED: basePipe Test Functions---------------------------\n";
+	}
+
+	std::cout << "Exiting basePipe template testing..." << std::endl;
+	return;
+}
+
+
+// TEST simplexArrayList Functions
+void t_pipe_base_functions(std::string &log, std::string type, std::string complexType){
+	
+	
+}
+
+
+// TEST simplexArrayList Functions
+void t_pipe_empty_functions(std::string &log, std::string type, std::string complexType){
+	std::string failLog = "";
+	auto *pack = new pipePacket(complexType, 5.0, 2);
+	basePipe *testPipe = new basePipe();
+	testPipe = testPipe->newPipe(type, complexType);
+	
+	std::map<std::string, std::string> testConfig;
+	
+	//Attempt to output pipe data without initializing pipe
+	// RET: void 
+	testPipe->outputData(*pack);
+	
+	//Attempt to run the pipe without initializing
+	// RET: void
+    testPipe->runPipe(*pack);
+    
+    //Configure the pipe with arguments
+    // RET: bool
+    if(!testPipe->configPipe(testConfig)){ failLog += "basePipe config failed\n"; }
+	
+	//Run the pipe wrapper with no selected pipe
+    testPipe->runPipeWrapper(*pack);
+	//Output log status to calling function
+	if(failLog.size() > 0){
+		log += "FAILED: " + type + " Empty Test Functions---------------------------\n" + failLog;	
+	} else {
+		 log += "PASSED: " + type + " Empty Test Functions---------------------------\n";
+	}
+	std::cout << "Exiting " + type + " empty function testing..." << std::endl;
+}
+
+int main (int, char**){
+	std::string log;
+	t_pipe_functions(log);
+	
+	
+	for(std::string type : {"distMatrix","neighGraph","rips","upscale","boundary","persistence","slidingwindow"}){ 
+		try{t_pipe_empty_functions(log, type, "simplexArrayList");}
+		catch(const std::exception){log += "FAILED: " + type + " Empty Test Functions---------------------------\n";}	
+		
+		try{t_pipe_base_functions(log, type, "simplexArrayList");}
+		catch(const std::exception){log += "FAILED: " + type + " Base Test Functions---------------------------\n";}	
+		
+	}
+	
+	std::cout << std::endl << log << std::endl;
+}
