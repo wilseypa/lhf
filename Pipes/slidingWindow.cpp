9df7769849cc4556c2ad310ed34a967a4f44077c
Anindya-Moitra <id.anindya@gmail.com>
2020-06-26 01:50:57 -0400

    Corrected some references to vector elements by pointers. The program is still segfaulting, but at some different point during the execution.

--- Pipes/slidingWindow.cpp
+++ Pipes/slidingWindow.cpp
@@ -57,9 +57,11 @@ void slidingWindow::deleteNNstats()
 
 void slidingWindow::updateStats()
 {
-
     // Delete the corresponding row from the distance matrix.
-    pPack->distMatrix.erase(pPack->distMatrix.begin() + defaultVals->indexToBeDeleted);
+    pPack->complex->distMatrix->erase(pPack->complex->distMatrix->begin() + defaultVals->indexToBeDeleted);
+
+    std::cout<< "Hello World: " << pPack->complex->distMatrix->size() << '\n';
+
 
     double sumOldNNdists = 0.0;
     double sumNewNNdists = 0.0;
@@ -76,13 +78,13 @@ void slidingWindow::updateStats()
     std::vector<int> tpIndices; // A vector to store the positions of the existing members of the target partition.
 
     // Add a new column and row to the end of the upper triangular distance matrix.
-    for(unsigned int i = 0; i < pPack->distMatrix.size(); i++)
+    for(unsigned int i = 0; i < pPack->complex->distMatrix->size(); i++)
     {
         // Delete the corresponding entry from each row.
-        pPack->distMatrix[i].erase( pPack->distMatrix[i].begin() + defaultVals->indexToBeDeleted );
+        pPack->complex->distMatrix->at(i).erase( pPack->complex->distMatrix->at(i).begin() + defaultVals->indexToBeDeleted );
 
         // Add the new distance value to the end of each row.
-        pPack->distMatrix[i].push_back( defaultVals->distsFromCurrVec[i] );
+        pPack->complex->distMatrix->at(i).push_back( defaultVals->distsFromCurrVec[i] );
 
         // Update NN statistics for only those partitions from which the point was deleted or to which the new point is to be added.
         // Case 1: The i-th point belongs to the partition the last point was deleted from, but not to the partition the new point is
@@ -107,18 +109,18 @@ void slidingWindow::updateStats()
                 std::vector<double> memberDistsFromVect;
                 std::vector<int> memberIndices;
 
-                for(unsigned int j = 0; j < pPack->distMatrix.size(); j++)
+                for(unsigned int j = 0; j < pPack->complex->distMatrix->size(); j++)
                 {
                     if ( defaultVals->partitionLabels[j] == defaultVals->labelToBeDeleted )
                     {
                         if (j < i)
                         {
-                            memberDistsFromVect.push_back( pPack->distMatrix[j][i] );
+                            memberDistsFromVect.push_back( pPack->complex->distMatrix->at(j).at(i) );
                             memberIndices.push_back(j);
                         }
                         else if (j > i)
                         {
-                            memberDistsFromVect.push_back( pPack->distMatrix[i][j] );
+                            memberDistsFromVect.push_back( pPack->complex->distMatrix->at(i).at(j) );
                             memberIndices.push_back(j);
                         }
                     }
@@ -224,12 +226,12 @@ void slidingWindow::updateStats()
                     {
                         if (j < i)
                         {
-                            memberDistsFromVect.push_back( pPack->distMatrix[j][i] );
+                            memberDistsFromVect.push_back( pPack->complex->distMatrix->at(j).at(i) );
                             memberIndices.push_back(j);
                         }
                         else if (j > i)
                         {
-                            memberDistsFromVect.push_back( pPack->distMatrix[i][j] );
+                            memberDistsFromVect.push_back( pPack->complex->distMatrix->at(i).at(j) );
                             memberIndices.push_back(j);
                         }
                     }
@@ -252,7 +254,7 @@ void slidingWindow::updateStats()
     }
 
     std::vector<double> distMatLastRow(defaultVals->windowMaxSize);  // The last row of the upper triangular distance matrix is a vector of 0s.
-    pPack->distMatrix.push_back( distMatLastRow );
+    pPack->complex->distMatrix->push_back( distMatLastRow );
 
     // Update the average NN distance of the partition from which the last point was deleted and of the one to which the new point
     // is being added.
