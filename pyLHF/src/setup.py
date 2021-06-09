0764db28d4937a0b36b4d1cfe7fa9c14a27c7a8e
malottno <malottno@mail.uc.edu>
2021-06-08 20:17:00 -0400

    Update version number, copy shared object before wheel build

--- pyLHF/src/setup.py
+++ pyLHF/src/setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='lhf',
-    version='1.0.4',    
+    version='1.0.5',    
     description='Light Weight Homology Framework',
     long_description=long_description,
     long_description_content_type="text/markdown",
