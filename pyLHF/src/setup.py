39fbd6a76abd94a1703d86a553ca47c123db8a24
Rishi Verma <verma.rishiraj@gmail.com>
2021-06-14 15:04:06 -0400

    Second attempt at platform-specific wheels

--- pyLHF/src/setup.py
+++ pyLHF/src/setup.py
@@ -11,11 +11,16 @@ from distutils.version import LooseVersion
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
+#Force package to be platform specific (since the shared object is pre-compiled, wheel tries to generate a pure-python wheel)
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 class bdist_wheel(_bdist_wheel):
     def finalize_options(self):
         _bdist_wheel.finalize_options(self)
         self.root_is_pure = False
+    def get_tag(self):
+        python, abi, plat = _bdist_wheel.get_tag(self)
+        python, abi = 'py3', 'none'
+        return python, abi, plat
 
 # class build_ext(_build_ext):
 with open("README.md", "r", encoding="utf-8") as fh:
