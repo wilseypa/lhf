8b035601d37e4377845022c8f3ad8d45b84c0c4a
ROhit <rohitpal21290@gmail.com>
2022-01-30 17:06:12 -0500

    Setting up routines for weight computaiton and filteration

--- python_tests/Polytopal_Development/Checbyshev_Centers.py
+++ python_tests/Polytopal_Development/Checbyshev_Centers.py
@@ -1,15 +1,38 @@
+#import numpy as np
+#import scipy.optimize as opt
+
+#matW = np.array([[-1,2],[4,-3]]).T
+#vecT = np.array([20,8])
+
+#rowNrmW = np.sqrt(np.sum(matW**2,axis=1))
+
+#matA = np.vstack((rowNrmW,matW.T)).T
+#vecB = vecT
+
+#vecC = np.array([0,-1,0])
+
+#res = opt.linprog(vecC,A_ub=matA,b_ub = vecB)
+#print(res)
 import numpy as np
-import scipy.optimize as opt
+import polytope as pc
+import pypoman.duality as poly
+A = np.array([[2, 1],
+              [2, -3],
+              [-1, 4],
+              [-3, -2],
+              [-1,1]])
 
-matW = np.array([[2,2,-3,1,-1],[1,-3,-2,-1,4]]).T
-vecT = np.array([22,8,-6,-2,20])
+b = np.array([22, 8, 20, -6,0])
 
-rowNrmW = np.sqrt(np.sum(matW**2,axis=1))
+p = pc.Polytope(A, b)
+#p = pc.box2poly([[0, 2], [0, 1]])
 
-matA = np.vstack((rowNrmW,matW.T)).T
-vecB = vecT
+p.dim # number of dimensions of ambient Euclidean space
+p.volume # measure in ambient space
+print(p.chebR) # Chebyshev ball radius
+print(p.chebXc) # Chebyshev ball center
+p.cheby
+p.bounding_box
 
-vecC = np.array([-1,0,0])
-
-res = opt.linprog(vecC,A_ub=matA,b_ub = vecB)
-print(res)
+x = poly.compute_polytope_halfspaces([[7.556,6.889],[6.667,6.667],[1.2,1.2],[2.615,-0.923],[9.25,3.5]])
+print(x)
--- python_tests/Polytopal_Development/PCA.py
+++ python_tests/Polytopal_Development/PCA.py
@@ -1,20 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import pypoman.duality as poly
+import polytope as pc
 
 from sklearn.decomposition import PCA
 
+fig, ax = plt.subplots()
 # creating the dataset
-x=np.array([[1,3,0],[2,2,0],[3,4,0],[4,2,0],[5,9,0]])
+x=np.array([[1,3,0],[2,2,0],[4,2,0],[5,9,0],[2.5,7,0]])
 plt.scatter(x[:,0], x[:,1])
+x43=np.array([[1,3,0],[2,2,0],[4,2,0],[5,9,0],[2.5,7,0],[1,3,0]])
+
 print(x)
 pca = PCA(n_components=3).fit(x)
 print(pca.components_)
 pca = PCA(n_components=2).fit(x)
 pca_x = pca.transform(x)
 print(pca.components_)
+plt.scatter(pca_x[:,0], pca_x[:,1], alpha=0.3)
+#cxfh
+
+x23 = poly.compute_polytope_halfspaces(pca_x)
+p23 = pc.Polytope(x23[0], x23[1])
+print(p23.chebR) # Chebyshev ball radius
+print(p23.chebXc) # Chebyshev ball center
+#dxfh
+print(type(pca_x))
+print(type(p23.chebXc))
+pca_x = np.append(pca_x,[p23.chebXc], axis=0)
+print(pca_x)
 x2 = pca.inverse_transform(pca_x)
 plt.scatter(x[:,0], x[:,1], alpha=0.3)
+plt.plot(x43[:,0],x43[:,1])
+plt.plot(pca_x[:,0],pca_x[:,1])
+
+print(x2)
 plt.scatter(x2[:,0], x2[:,1])
+circle1 = plt.Circle((2.73301,3.46085), 1.46085185, color='r')
+circle2 = plt.Circle((-1.13685,0.18198), 1.46085185, color='r')
+ax.add_patch(circle1)
+ax.add_patch(circle2)
+plt.xlim(-3, 3)
+plt.ylim(-3, 3)
+plt.axis('equal')
 plt.show()
 print(pca_x)
