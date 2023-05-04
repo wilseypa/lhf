b6a7e0933fca7ea574269719f7da3e8940adf01c
ROhit <rohitpal21290@gmail.com>
2023-05-04 06:19:05 -0400

    minor updates

--- Complex/polytopalcomplex.cpp
+++ Complex/polytopalcomplex.cpp
@@ -124,10 +124,12 @@ vector<vector<double>> projectpointsOnUnitdSphere(vector<vector<double>> &inputD
 }
 
 
-vector<unsigned> findOppositeSimplex(pair<vector<vector<double>>,vector<vector<double>>> projectionData,vector<vector<unsigned>> hull){
-	vector<double> point = projectionData.second[0];
-	double minweight = 9999;
-	unsigned projectionSimplex;
+pair<vector<unsigned>,unsigned> findOppositeSimplex(pair<vector<vector<double>>,vector<vector<double>>> projectionData,vector<vector<unsigned>> hull){
+	vector<double> point1 = projectionData.second[0];
+	vector<double> point2 = projectionData.second[1];
+	double minweight1 = 9999;
+	double minweight2 = 9999;
+	unsigned projectionSimplex1,projectionSimplex2;
 	int i=0;
 	for(auto x : hull){
 		vector<double> centroid(projectionData.first[0].size(),0.0);
@@ -136,14 +138,22 @@ vector<unsigned> findOppositeSimplex(pair<vector<vector<double>>,vector<vector<d
 				centroid[j] +=((projectionData.first[y][j])/projectionData.first[0].size());
 			}
 		}
-		auto d = utils::vectors_distance(centroid,point);
-		if(d<minweight){
-			minweight = d;
-			projectionSimplex = i; 
+		auto d1 = utils::vectors_distance(centroid,point1);
+		if(d1<minweight1){
+			minweight1 = d1;
+			projectionSimplex1 = i; 
+		}
+		auto d2 = utils::vectors_distance(centroid,point2);
+		if(d2<minweight2){
+			minweight2 = d2;
+			projectionSimplex2 = i; 
 		}
 		i++;
 	}
-	return hull[projectionSimplex];
+	if(minweight1<minweight2)
+		return make_pair(hull[projectionSimplex1],0);
+		
+	return make_pair(hull[projectionSimplex2],1);
 }
 
 pair<vector<vector<double>>,vector<vector<double>>> transformingConvexPolytopeForConvexDecomposition(vector<vector<double>> &inputData,vector<unsigned> projectionSimplex){
@@ -167,40 +177,110 @@ pair<vector<vector<double>>,vector<vector<double>>> transformingConvexPolytopeFo
 	modifiedPoints.push_back(pp1);
 	modifiedPoints.push_back(pp2);
 	auto newpoints  = utils::computePCAInverse(pts,modifiedPoints,pca.second);
-	for(auto f:newpoints){
-		for(auto d:f)
-			cout<<d<<" ";
-		cout<<"\n";
-	}
-	
 	pp1 = newpoints[newpoints.size()-2];
 	pp2 = newpoints[newpoints.size()-1];
 	auto trasformedData = utils::projectHSphere(inputData,center);
 	pp.push_back(pp1);
 	pp.push_back(pp2);
+	pp.push_back(center);
 	return make_pair(trasformedData,pp);
 }
-projectOnSimplexPlane(pair<vector<vector<double>>,vector<vector<double>>> projectionData,vector<vector<unsigned>> hull){
+/*
+def projectonplane(p1,p2,points1):
+	cofficient = p1 - p2
+	constant  = 0
+	point_on_plane = p1
+	for a,b in zip(point_on_plane,cofficient):
+		constant = constant+a*b
+	constant = (-1)*constant 
 	
+	pts1 = []
+	for pt in points1:
+		diff = pt-p2
+		constterm =0;
+		diffterm = 0;
+		for x in range(0,len(cofficient)):
+			constterm+= p2[x]*cofficient[x]
+			diffterm+= diff[x]*cofficient[x]
+		if((diffterm!=0).all()):
+			t = ((-1)*constant - constterm)/diffterm
+			pnt = t*(pt-p2)+p2
+			pts1.append(pnt)
+		else:
+			pts1.append(pt)
+	pts1 = np.array(pts1)
+	return pts1
+*/
+
+vector<vector<double>> projectOnSimplexPlane(pair<vector<vector<double>>,vector<vector<double>>> projectionData,vector<unsigned> hull,vector<double> ppoint){
+	double scaledown = 100000;
+	vector<double> pp;
+	double d=utils::vectors_distance(ppoint,projectionData.second[2]);
+	for(int i =0;i<ppoint.size();i++)
+		pp.push_back(projectionData.second[2][i]+((ppoint[i]-projectionData.second[2][i])/(d*scaledown)));
+	vector<double> normal;
+	for(int i=0;i<projectionData.second[0].size();i++)
+		normal.push_back(projectionData.second[0][i]-projectionData.second[1][i]);
 	
+	double constant  = 0;
+	auto point_on_plane = projectionData.second[2];
+	for (int i=0;i<point_on_plane.size();i++){
+		constant += point_on_plane[i]*normal[i];
+	}
+	constant = (-1)*constant;
 	
-	return ProjectedData;
+	vector<vector<double>> pts1;
+	for (auto pt :projectionData.first){
+		vector<double> diff;
+		for(int i=0;i<pt.size();i++)
+			diff.push_back(pt[i]-pp[i]);
+		double constterm = 0;
+		double diffterm = 0;
+		for(int x=0;x<normal.size();x++){
+			constterm+= pp[x]*normal[x];
+			diffterm+= diff[x]*normal[x];
+		}
+		if(diffterm!=0){
+			double t = ((-1)*constant - constterm)/diffterm;
+			vector<double> pnt;
+			for(int i=0;i<pt.size();i++){
+				pnt.push_back(t*(pt[i]-pp[i])+pp[i]);
+			}
+			pts1.push_back(pnt);
+		}
+		else
+			pts1.push_back(pt);
+	}
+	auto pts = utils::computePCA(pts1,pts1[0].size()-1);
+	return pts.first;
 }
 
 polytopalComplex :: polytopalComplex(vector<vector<double>> &inputData){
+	int k;
 	populateDistanceMatrix(inputData);
 	auto simplices = qdelaunay_o(inputData);
 	auto hull = hullfromtriangulation(simplices);
 	auto weight1 = getweight(hull[0]);
 	auto projectionData1 = transformingConvexPolytopeForConvexDecomposition(inputData,hull[0]);
-	auto projectedonPlane1 = projectOnSimplexPlane(projectionData1,hull[0]);
 	auto oppositeSimplex = findOppositeSimplex(projectionData1,hull);
-	cout<<hull[0][0]<<" "<<hull[0][1]<<" "<<hull[0][2]<<"\n";
-	cout<<oppositeSimplex[0]<<" "<<oppositeSimplex[1]<<" "<<oppositeSimplex[2];
-	int k;
-	cin>>k;
-	auto weight2 = getweight(oppositeSimplex);
-	auto projectionData2 = transformingConvexPolytopeForConvexDecomposition(inputData,oppositeSimplex);
+	vector<double> pp1,pp2;
+	if(oppositeSimplex.second==0)
+		pp1 = projectionData1.second[1];
+	else
+		pp1 = projectionData1.second[0];
+
+	auto projectedonPlane1 = projectOnSimplexPlane(projectionData1,hull[0],pp1);
+	//cout<<hull[0][0]<<" "<<hull[0][1]<<" "<<hull[0][2]<<"\n";
+	//cout<<oppositeSimplex[0]<<" "<<oppositeSimplex[1]<<" "<<oppositeSimplex[2];
+	
+	//cin>>k;
+	auto weight2 = getweight(hull[oppositeSimplex.second]);
+	auto projectionData2 = transformingConvexPolytopeForConvexDecomposition(inputData,oppositeSimplex.first);
+	if(utils::vectors_distance(pp1,projectionData2.second[0])<utils::vectors_distance(pp1,projectionData2.second[1]))
+		pp2 = projectionData2.second[1];
+	else
+		pp2 = projectionData2.second[0];
+	auto projectedonPlane2 = projectOnSimplexPlane(projectionData2,oppositeSimplex.first,pp2);
 
 	bool first = true;
 	for(int i=0;i<projectionData1.first.size();i++){
@@ -225,6 +305,36 @@ polytopalComplex :: polytopalComplex(vector<vector<double>> &inputData){
 			}
 			else
 				cout<<","<<projectionData2.first[i][j];
+		}
+		cout<<"\n";
+		first = true;
+
+	}
+	first = true;
+	cout<<"\n\n";
+	for(int i=0;i<projectedonPlane1.size();i++){
+		for(int j =0;j<projectedonPlane1[i].size();j++){
+			if(first){
+				cout<<projectedonPlane1[i][j];
+				first=false;
+			}
+			else
+				cout<<","<<projectedonPlane1[i][j];
+		}
+		cout<<"\n";
+		first = true;
+
+	}
+	first = true;
+	cout<<"\n\n";
+	for(int i=0;i<projectedonPlane2.size();i++){
+		for(int j =0;j<projectedonPlane2[i].size();j++){
+			if(first){
+				cout<<projectedonPlane2[i][j];
+				first=false;
+			}
+			else
+				cout<<","<<projectedonPlane2[i][j];
 		}
 		cout<<"\n";
 		first = true;
