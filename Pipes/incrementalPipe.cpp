ad75a1070001a6fbbafad9dd839c5c0ff9d17fde
Anurag Yadav <yanurag994@gmail.com>
2023-07-24 23:59:31 -0400

    Code cleanup

--- Pipes/incrementalPipe.cpp
+++ Pipes/incrementalPipe.cpp
@@ -6,31 +6,6 @@
 #include <chrono>
 
 template <typename T>
-std::ostream &operator<<(std::ostream &os, const std::vector<T> &vec)
-{
-	os << "[ ";
-	for (const auto &elem : vec)
-	{
-		os << elem << ", ";
-	}
-	os << "]";
-	return os;
-}
-
-template <typename T>
-std::ostream &operator<<(std::ostream &os, const std::set<T> &set)
-{
-	os << "{ ";
-	for (const auto &elem : set)
-	{
-		os << elem << ",";
-	}
-	os << "}";
-	return os;
-}
-
-
-template <typename T>
 std::vector<T> operator-(const std::vector<T> &a, const std::vector<T> &b) // Vector Subtraction
 {
 	std::vector<T> temp;
@@ -40,7 +15,7 @@ std::vector<T> operator-(const std::vector<T> &a, const std::vector<T> &b) // Ve
 }
 
 template <typename T>
-T dot(const std::vector<T> &a, const std::vector<T> &b) //Vector Dot Product
+T dot(const std::vector<T> &a, const std::vector<T> &b) // Vector Dot Product
 {
 	std::vector<T> temp;
 	std::transform(a.begin(), a.end(), b.begin(), std::back_inserter(temp), [](T e1, T e2)
@@ -48,61 +23,19 @@ T dot(const std::vector<T> &a, const std::vector<T> &b) //Vector Dot Product
 	return std::accumulate(temp.begin(), temp.end(), 0.0);
 }
 
-int bruteforce(std::vector<short> simp, std::vector<std::vector<double>> &inputData, short &omission)
-{
-	std::set<unsigned> simplex(simp.begin(),simp.end());
-	for (unsigned i = 0; i < inputData.size(); i++)
-	{
-		if (simplex.find(i) != simplex.end() || i == omission)
-			continue;
-		simplex.insert(i);
-		auto center = utils::circumCenter(simplex, inputData);
-		auto radius = utils::vectors_distance(center, inputData[i]);
-		unsigned point;
-		for (point = 0; point < inputData.size(); point++)
-		{
-			if (simplex.find(point) == simplex.end() && utils::vectors_distance(center, inputData[point]) < radius)
-				break;
-		}
-		if (point == inputData.size())	return i;
-		simplex.erase(i);
-	}
-	return -1;
-}
-
 template <typename nodeType>
-short incrementalPipe<nodeType>::validate(std::vector<short>& simp, short &triangulation_point, short &omission)
-{
-	simp.push_back(triangulation_point);
-	std::vector<double> center = utils::circumCenter(simp, this->inputData);
-	double radius = utils::vectors_distance(center, this->inputData[*simp.begin()]);	
-	for (short point = 0; point < this->data_set_size; point++)
-	{
-		if (std::find(simp.begin(), simp.end(), point) == simp.end() && utils::vectors_distance(center, this->inputData[point]) < 0.99999 * radius)
-		{
-			simp.pop_back();
-			std::cout<<"Invalidated"<<std::endl;
-			return bruteforce(simp, this->inputData, omission);
-		}
-	}
-	simp.pop_back();
-	return triangulation_point;
-}
-
-template <typename nodeType>
-std::vector<double> incrementalPipe<nodeType>::solvePlaneEquation(const std::vector<short> &points)
+std::vector<double> incrementalPipe<nodeType>::solvePlaneEquation(const std::vector<short> &points) // Solution to equation of a hyperplane
 {
 	int numPoints = points.size();
 	Eigen::MatrixXd A(numPoints, this->dim + 1);
-	Eigen::VectorXd B(numPoints);
+	;
 	for (int i = 0; i < numPoints; i++)
 	{
 		for (int j = 0; j < this->dim; j++)
 			A(i, j) = this->inputData[points[i]][j];
-		A(i, this->dim) = 0;
-		B(i) = 1;
 	}
-	Eigen::VectorXd coefficients = A.completeOrthogonalDecomposition().solve(B);
+	A.col(this->dim).setConstant(0);
+	Eigen::VectorXd coefficients = A.completeOrthogonalDecomposition().solve(Eigen::VectorXd::Ones(numPoints));
 	return std::vector<double>(coefficients.data(), coefficients.data() + coefficients.size());
 }
 
@@ -111,29 +44,27 @@ std::vector<short> incrementalPipe<nodeType>::first_simplex()
 {
 	std::vector<short> simplex;
 	if (this->data_set_size < this->dim + 2)
-		return simplex; //Not enough points
-	for(short i=0;i < this->dim;i++)
-		simplex.push_back(i); //Pseudo Random initialization of Splitting Hyperplane
-	std::vector<short> outer_points;
+		return simplex; // Not enough points
+	for (short i = 0; i < this->dim; i++)
+		simplex.push_back(i); // Pseudo Random initialization of Splitting Hyperplane
 	auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
 	auto rng = std::default_random_engine{};
+	std::vector<short> outer_points;
 	do
 	{
-		for (auto i : outer_points)
-			simplex.push_back(i);
+		simplex.insert(simplex.end(), outer_points.begin(), outer_points.end());
 		std::shuffle(simplex.begin(), simplex.end(), rng);
 		outer_points.clear();
-		while (simplex.size() != this->dim)
-			simplex.pop_back();
+		simplex.erase(simplex.begin() + this->dim, simplex.end());
 		auto equation = solvePlaneEquation(simplex);
 		for (unsigned i = 0; i < this->data_set_size; i++)
 			if (std::find(simplex.begin(), simplex.end(), i) == simplex.end() && dot(this->inputData[i], equation) > 1)
 				outer_points.push_back(i);
-	} while (!outer_points.empty()); //Converge Hyperplane to Convex Hull
+	} while (!outer_points.empty()); // Converge Hyperplane to Convex Hull
 	double radius = 0;
 	std::vector<double> center;
 	std::set<unsigned> simplex_set(simplex.begin(), simplex.end());
-	for (unsigned i = 0; i < this->data_set_size; i++) //BruteForce to Find last point for construction of simplex.
+	for (unsigned i = 0; i < this->data_set_size; i++) // BruteForce to Find last point for construction of simplex.
 	{
 		if (simplex_set.find(i) != simplex_set.end())
 			continue;
@@ -159,43 +90,46 @@ std::vector<short> incrementalPipe<nodeType>::first_simplex()
 }
 
 template <typename nodeType>
-void incrementalPipe<nodeType>::cospherical_handler(std::vector<short> &simp, int &tp, short &omission, std::vector<std::vector<double>>& distMatrix){
+void incrementalPipe<nodeType>::cospherical_handler(std::vector<short> &simp, int &tp, short &omission, std::vector<std::vector<double>> &distMatrix)
+{
 	auto triangulation_point = tp;
-	auto temp=simp;
+	auto temp = simp;
 	temp.push_back(triangulation_point);
-	std::sort(temp.begin(),temp.end());
+	std::sort(temp.begin(), temp.end());
 	this->dsimplexes.insert(temp);
-	for(auto i : temp)
+	for (auto i : temp)
 	{
-		auto new_face=temp;
-		new_face.erase(std::find(new_face.begin(),new_face.end(),i));
-		if(simp==new_face) continue;
-		auto new_point = expand_d_minus_1_simplex(new_face,i,distMatrix);
-		if(new_point == -1) continue;
+		auto new_face = temp;
+		new_face.erase(std::find(new_face.begin(), new_face.end(), i));
+		if (simp == new_face)
+			continue;
+		auto new_point = expand_d_minus_1_simplex(new_face, i, distMatrix);
+		if (new_point == -1)
+			continue;
 		new_face.push_back(new_point);
-		std::sort(new_face.begin(),new_face.end());
+		std::sort(new_face.begin(), new_face.end());
 		this->dsimplexes.insert(new_face);
 	}
 }
 
 template <typename nodeType>
-int incrementalPipe<nodeType>::expand_d_minus_1_simplex(std::vector<short> &simp, short &omission, std::vector<std::vector<double>>& distMatrix)
+int incrementalPipe<nodeType>::expand_d_minus_1_simplex(std::vector<short> &simp, short &omission, std::vector<std::vector<double>> &distMatrix)
 {
 	auto normal = solvePlaneEquation(simp);
 	auto p1 = utils::circumCenter(simp, this->inputData);
 	bool direction = (dot(normal, this->inputData[omission]) > 1);
 	std::vector<double> radius_vec(this->data_set_size, 0);
-	double largest_radius = 0, smallest_radius = std::numeric_limits<double>::max()/2, ring_radius = utils::vectors_distance(p1, inputData[simp[0]]);
+	double largest_radius = 0, smallest_radius = std::numeric_limits<double>::max() / 2, ring_radius = utils::vectors_distance(p1, inputData[simp[0]]);
 	int triangulation_point = -1;
 	bool flag = true;
-	std::set<short> simplex(simp.begin(),simp.end());
+	std::set<short> simplex(simp.begin(), simp.end());
 	simplex.insert(omission);
 	for (auto &new_point : this->search_space)
 	{
-		if (simplex.find(new_point)==simplex.end() && (direction ^ dot(normal, inputData[new_point]) > 1))
+		if (simplex.find(new_point) == simplex.end() && (direction ^ dot(normal, inputData[new_point]) > 1))
 		{
 			simp.push_back(new_point);
-			auto temp=utils::vectors_distance(p1, inputData[new_point]);
+			auto temp = utils::vectors_distance(p1, inputData[new_point]);
 			if (ring_radius > temp)
 			{
 				radius_vec[new_point] = sqrt(utils::circumRadius(simp, distMatrix));
@@ -206,7 +140,7 @@ int incrementalPipe<nodeType>::expand_d_minus_1_simplex(std::vector<short> &simp
 					flag = false;
 				}
 			}
-			else if (flag && 2*smallest_radius > temp) //reduce search space
+			else if (flag && 2 * smallest_radius > temp) // reduce search space
 			{
 				radius_vec[new_point] = sqrt(utils::circumRadius(simp, distMatrix));
 				if (smallest_radius > radius_vec[new_point])
@@ -224,8 +158,9 @@ int incrementalPipe<nodeType>::expand_d_minus_1_simplex(std::vector<short> &simp
 		double triangulation_radius = radius_vec[triangulation_point];
 		count = std::count_if(radius_vec.begin(), radius_vec.end(), [triangulation_radius](double val)
 							  { return std::abs(1 - (val / triangulation_radius)) <= 0.000000000001; });
-		if (count != 1){
-			//cospherical_handler(simp,triangulation_point,omission,distMatrix);
+		if (count != 1)
+		{
+			// cospherical_handler(simp,triangulation_point,omission,distMatrix);
 			return -1;
 		}
 	}
@@ -234,7 +169,7 @@ int incrementalPipe<nodeType>::expand_d_minus_1_simplex(std::vector<short> &simp
 
 // basePipe constructor
 template <typename nodeType>
-incrementalPipe<nodeType>::incrementalPipe()  : inputData(*(new std::vector<std::vector<double>>()))
+incrementalPipe<nodeType>::incrementalPipe() : inputData(*(new std::vector<std::vector<double>>()))
 {
 	this->pipeType = "incrementalPipe";
 	return;
@@ -271,22 +206,18 @@ void incrementalPipe<nodeType>::runPipe(pipePacket<nodeType> &inData)
 			continue;
 		first_vector.push_back(new_point);
 		std::sort(first_vector.begin(), first_vector.end());
-		if(!this->dsimplexes.insert(first_vector).second) continue;
-		for (auto& i: first_vector)
+		if (!this->dsimplexes.insert(first_vector).second)
+			continue;
+		for (auto &i : first_vector)
 		{
 			if (i == new_point)
 				continue;
 			std::vector<short> key = first_vector;
 			key.erase(std::find(key.begin(), key.end(), i));
-			auto temp=this->inner_d_1_shell.emplace(key, i);
+			auto temp = this->inner_d_1_shell.emplace(key, i);
 			if (!temp.second)
 				this->inner_d_1_shell.erase(temp.first); // Create new shell and remove collided faces max only 2 can occur.
 		}
-/*  	if (this->inner_d_1_shell.size() > 2000000) {
-		std::cout << "Flushing at " << dsimplexes.size() << std::endl;
-		auto eraseStart = std::next(this->inner_d_1_shell.begin(),  2000000*0.10);
-		this->inner_d_1_shell.erase(eraseStart, this->inner_d_1_shell.end());
-	} */
 	}
 	std::cout << dsimplexes.size() << std::endl;
 	return;
--- Pipes/incrementalPipe.hpp
+++ Pipes/incrementalPipe.hpp
@@ -14,14 +14,13 @@ class incrementalPipe : public basePipe<nodeType> {
     std::vector<short> search_space;
     unsigned dim;
     unsigned data_set_size;
-  public:
-    incrementalPipe();
-    void runPipe(pipePacket<nodeType>& inData);
     std::vector<double> solvePlaneEquation(const std::vector<short> &points);
-    short validate(std::vector<short>& simp, short &triangulation_point, short &omission);
     std::vector<short> first_simplex();
     void cospherical_handler(std::vector<short> &simp, int &tp, short &omission, std::vector<std::vector<double>>& distMatrix);
     int expand_d_minus_1_simplex(std::vector<short> &simp_vector, short &omission, std::vector<std::vector<double>>& distMatrix);
+  public:
+    incrementalPipe();
+    void runPipe(pipePacket<nodeType>& inData);
     bool configPipe(std::map<std::string, std::string> &configMap);
     void outputData(pipePacket<nodeType>&);
     // std::vector<std::vector<int>> qdelaunay_o(const delaunay &delaunay);
