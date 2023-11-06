9f88f34eb40b149331d6f9c9c8072482113a7475
Liu-Yanxiao <liu5y8@mail.uc.edu>
2023-11-06 14:07:09 -0500

    JCI students working on this project

--- Complex/witnessComplex.cpp
+++ Complex/witnessComplex.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include "witnessComplex.hpp"
 
+// Yanxio , Yanlin and  Jisun are working on witness complex and will post there code here.
+
 template<typename nodeType>
 witnessComplex<nodeType>::witnessComplex(double maxE, double maxD) : simplexArrayList<nodeType>::simplexArrayList(0,0) {
 	this->simplexType = "alphaComplex";
