c239952c4d11d60213138181983a5993cb74ae34
rohitpalsingh <79603501+rohitpalsingh@users.noreply.github.com>
2026-03-31 17:42:55 +0530

    Create directPolytopal class interface
    Added directPolytopal class inheriting from basePipe.

--- Pipes/directPolytopal.hpp
+++ Pipes/directPolytopal.hpp
@@ -1,1 +1,15 @@
-//Interface
+#pragma once
+
+#include "basePipe.hpp"
+
+// basePipe constructor
+template <typename nodeType>
+class directPolytopal : public basePipe<nodeType>
+{
+private:
+public:
+  directPolytopal();
+  void runPipe(pipePacket<nodeType> &inData);
+  bool configPipe(std::map<std::string, std::string> &configMap);
+  void outputData(pipePacket<nodeType> &);
+};
